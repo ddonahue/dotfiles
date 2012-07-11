4c4b47c604373879e0d7ecc0d486572e110a7993
Dan Donahue <ddonahue215@gmail.com>
2012-07-11 09:25:13 -0400

    Updated link script to use absolute path of files and to fix duplicated symbolic links for folders.

--- link_dotfiles.rb
+++ link_dotfiles.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/env ruby
 
 # from http://errtheblog.com/posts/89-huba-huba
+dotfiles_dir = File.dirname(__FILE__)
+home_dir = File.expand_path('~')
 
-home = File.expand_path('~')
-dot_files = ['bash_profile','bashrc','bash', 'gvimrc', 'vimrc','gitconfig','git-completion.bash', 'zshrc', 'zsh']
+dotfiles = ['bash_profile','bashrc','gvimrc','vimrc','gitconfig','git-completion.bash','zshrc', 'bash', 'zsh']
 
-dot_files.each do |file|
-  target = File.join(home, ".#{file}")
-  `ln -s #{File.expand_path file} #{target}`
+dotfiles.each do |file|
+  source = File.expand_path(File.join(dotfiles_dir, file))
+  target = File.expand_path(File.join(home_dir, ".#{file}"))
+
+  if (File.directory?(source) && File.exist?(target))
+    File.delete(target)
+  end
+
+  `ln -s #{source} #{target}`
 end
 
 # git push on commit
